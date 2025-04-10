67ddc536657e126aaadd35f144302b900d9e4d10
eliasalvarado <77988653+eliasalvarado@users.noreply.github.com>
2025-04-09 18:33:15 -0600

    Generación de llaves

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,8 +1,10 @@
 const express = require("express");
-const { login, register } = require("../controllers/auth.controller");
+const { login, register, generateKeys } = require("../controllers/auth.controller");
 const router = express.Router();
+const authenticate = require("../middlewares/auth.middleware");
 
 router.post("/login", login);
 router.post("/register", register);
+router.post("/generate-keys", authenticate, generateKeys);
 
 module.exports = router;
